942dfa4cbb3ccf4834e197e94b44fc0f807de9ba
Iffat <iffatsattar6@gmail.com>
2025-05-21 12:31:13 -0700

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function Footer () {
+    const currentyear = new Date().getFullYear();
+    return (
+        <footer>
+           <p> Copyright ⓒ {currentyear} </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header () {
+    return(
+  <header>
+  <h1>MindPad</h1>
+  <p className="tagline">A clean space for messy minds.</p>
+  </header>
+    );
+}
+
+export default Header;
